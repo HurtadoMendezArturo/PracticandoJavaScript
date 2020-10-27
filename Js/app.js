7bd313ff77418e359126cdba41c1362abd44e434
ArturoHM <hurtadomendezarturo@gmail.com>
2020-10-26 20:28:12 -0500

    cadena de texto

--- Js/app.js
+++ Js/app.js
@@ -1,1 +1,22 @@
-console.log('Hola mundo');
+/*Cadena de texto*/
+
+let nombre = "Arturo";
+let apellido = "H-M";
+let saludo = new String("Hola mundo");
+let lorem = "lorem ipsum dudbbb uhuioohffl huhduhduf ipsum  dyufgcv vvfrts uusgdg ipsum ddjjndbbfbd hchduc ipsum  gfdjhdeídfg";
+
+
+console.log(nombre, apellido, saludo);
+
+console.log(
+    nombre.length,
+    apellido.length,
+    saludo.length,
+    nombre.toUpperCase(),
+    apellido.toLowerCase(),
+    lorem.includes("ipsum"),
+    lorem.includes("Arturo"),
+    lorem.trim(),
+    lorem.split(""),
+    lorem.split(",")
+); 
