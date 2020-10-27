a8ea18a16dc9f72daa47a24858d311ba292af652
ArturoHM <hurtadomendezarturo@gmail.com>
2020-10-27 14:20:47 -0500

    Primer ejercicio

--- Js/If-Else.js
+++ Js/If-Else.js
@@ -1,1 +1,10 @@
-alert('ajsnodn')
+
+let respuesta = prompt("ES CULPABLE RESPONDA SI - NO");
+
+if(respuesta == "si") {
+    document.write(`EL acusado respondio ${respuesta} es culpable`);
+}else if (respuesta == "no") {
+    document.write(`El acusado respondio ${respuesta} es inocente`);
+}else{
+    document.write("Solo puede responder son SI - NO");
+}
