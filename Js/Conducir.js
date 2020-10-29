e5d73a854e34b227e4d5fd7294530d6c9a4f8ae1
Arturo <hurtadomendezarturo@gmail.com>
2020-10-29 16:17:24 -0500

    Ejercicio 3

---
+++ Js/Conducir.js
@@ -0,0 +1,9 @@
+let edad = prompt("Cuantos años tienes");
+
+if (edad >= 18) {
+    document.write("Eres mayor de edad ya pudes conducir")
+}else if (edad <=17 && edad >=0) {
+    document.write("Eres menor de edad no puedes conducir");
+} else {
+    document.write("No has ingresado una edad");
+}
