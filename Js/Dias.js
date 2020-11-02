85cc7818b71ce6a49388eb8afc4c9586fe69a7a2
Arturo <hurtadomendezarturo@gmail.com>
2020-11-02 11:53:49 -0500

    Ejercicio 8

---
+++ Js/Dias.js
@@ -0,0 +1,11 @@
+let dia_semana = prompt ("Ingresa un día de la semana");
+
+if (dia_semana == "lunes") {
+    document.write("Es el primer dia de la samana");
+}else if (dia_semana == "viernes") {
+    document.write("Es fin de la semana");
+}else if (dia_semana == "sabado" || dia_semana == "domingo") {
+    document.write("Es dia de descanzo");
+}else{
+    document.write("Es hora de trabajar");
+} 
