7fcb92d7226900cd84e7608595acb314a1b1dff5
Arturo <hurtadomendezarturo@gmail.com>
2020-11-02 11:37:58 -0500

    Ejericio 6

---
+++ Js/NumerosIguales.js
@@ -0,0 +1,10 @@
+let numero_uno = prompt("Ingresa el primer número");
+let numero_dos = prompt("Ingresa el segundo número");
+
+if (numero_uno < numero_dos) {
+    document.write("El numero uno es menor que el numero dos");
+}else if (numero_dos < numero_uno) {
+    document.write("El numero dos es menor que el numero uno");
+} else {
+    document.write("Ingresaste mal el numero");
+} 
