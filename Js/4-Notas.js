417a8134d230e83b075234448ea40ec4a86a6612
Arturo <hurtadomendezarturo@gmail.com>
2020-11-02 12:34:10 -0500

    Ejercicio 4

--- Js/4-Notas.js
+++ Js/4-Notas.js
@@ -15,38 +15,3 @@ if (nota == 0 || nota <= 3) {
 }else {
     document.write("Ingresaste mal la nota")   ;
 } 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-if (nota = 0 || nota <= 3) {
-    document.write("Muy deficiente");
-
-}else if (nota = 4 || nota <= 5) {
-    document.write("Insuficiente");
-
-}else if (nota = 6 || nota <= 7) {
-    document.write("Suficiente");
-
-}else if (nota = 8 || nota <= 9) {
-    document.write("Bien");
-
-}else if (nota >= 10) {
-    document.write("Sobresaliente")
-
-}else {
-    document.write("No haz ingresado bien la nota");
-}
-*/
