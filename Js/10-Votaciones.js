f23594caf6a68745cfd70b740669b4c1d1f1f189
Arturo <hurtadomendezarturo@gmail.com>
2020-11-02 14:23:48 -0500

    Ejercicio 10

---
+++ Js/10-Votaciones.js
@@ -0,0 +1,11 @@
+let vota = prompt ("Debes votar por uno de los siguientes eventos: A: Equipo Rojo B: Equipo Verde C: Equipo Azul")
+
+if (vota === "a") {
+    document.write("Haz votado por el equipo ROJO");
+}else if (vota === "b") {
+    document.write("Haz votado por el equipo VERDE");
+}else if (vota === "c") {
+    document.write("Haz votado por el equipo AZUL")
+} else {
+    document.write("Opción errada")  ;
+} 
