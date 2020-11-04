06dfaff4c0a8f9932151c5b89091745154870699
Coding With R2 <codingwithr2@gmail.com>
2020-11-04 12:42:50 -0500

    Ejercicio 15

---
+++ Js/15-Mayoede100.js
@@ -0,0 +1,7 @@
+let numero = prompt("Ingrese un número");
+
+if (numero > 100) {
+    document.write("El número ingresado es mayor a 100");
+}else{
+    document.write("El número ingresado es menor a 100");
+}
