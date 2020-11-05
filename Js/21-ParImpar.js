f58669f23f6f6d6dd692c35e204f774000a4c052
Coding With R2 <codingwithr2@gmail.com>
2020-11-05 15:46:22 -0500

    Ejercicio 21

---
+++ Js/21-ParImpar.js
@@ -0,0 +1,8 @@
+let numero = prompt("Ingrese un número parasaber si era impar");
+
+
+if (numero % 2 == 0) {
+    document.write(`El número ${numero} es par`);
+} else {
+    document.write(`El número ${numero} es impar`);
+}
