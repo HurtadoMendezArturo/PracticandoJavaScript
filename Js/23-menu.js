b26fe0767703c20f2814d6a779288c7d9f13d958
Coding With R2 <codingwithr2@gmail.com>
2020-11-05 16:16:37 -0500

    Ejercicio 23

---
+++ Js/23-menu.js
@@ -0,0 +1,14 @@
+let menu = prompt ("Ingrese una de las siguientes opciones\nARCHIVO\nBUSCAR\nSALIR");
+
+if (menu == "archivo" || menu == "ARCHIVO") {
+    document.write(`Escogiste la opción ${menu}`);
+
+}else if (menu == "buscar" || menu == "BUSCAR") {
+    document.write(`Escogiste la opción ${menu}`);
+
+}else if (menu == "salir" || menu == "SALIR") {
+    document.write(`Escogiste la opción ${menu}`);
+
+} else {
+    document.write("Escogiste una opción erronea");
+} 
